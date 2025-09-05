6b94fc2d292072b0367a0f1342500ca65c0a059d
yji <dksro333@naver.com>
2025-09-05 17:59:13 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
+/* eslint-disable */
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let post = '글제목입니다.';
+//   let [글제목, b] = useState('내일의 날짜 입니다.');//변경시 자동재랜더링
+  let [글제목, b] = useState(['오늘의 날짜 입니다.', '내일의 날짜 입니다.', '어제의 날짜 입니다']);//변경시 자동재랜더링
+  let [logo, setLogo] = useState('React Blog');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+        <div className='title'>
+            <h3>{ logo }</h3>
+        </div>
+        <div className="list">
+            <h4>{ 글제목[0] } <span>좋아요</span> 0 </h4>
+            <p>2025-09-05</p>
+        </div>
+        <div className="list">
+            <h4>{ 글제목[1] }</h4>
+            <p>2025-09-06</p>
+        </div>
+        <div className="list">
+            <h4>{ 글제목[2] }</h4>
+            <p>2025-09-04</p>
+        </div>
+    </div>
   )
 }
 
